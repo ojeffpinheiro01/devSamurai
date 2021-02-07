cbc918657539c125192a0444db28665890064d9a
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-02-07 13:38:32 -0300

    estilização inicial

--- frontend/react/todo/src/App.js
+++ frontend/react/todo/src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <h1>App</h1>
-  );
-}
+import "./App.css";
+
+const App = () => <h1 className="title" >App</h1>;
 
 export default App;
