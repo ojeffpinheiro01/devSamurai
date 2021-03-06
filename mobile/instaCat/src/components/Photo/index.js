d7d09a17f038a1f3d76e1150edc1f0be493aeb1f
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-03-06 16:14:57 -0300

    add img

--- mobile/instaCat/src/components/Photo/index.js
+++ mobile/instaCat/src/components/Photo/index.js
@@ -1,35 +1,44 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const Photo = () => {
-    const [like, setLike] = useState(0);
+const Photo = ({ file, count }) => {
+  const [like, setLike] = useState(count);
 
-    const increment = () => {
-        setLike(like + 1)
-    }
-    return (
-
-        <View style={styles.container} >
-            <TouchableOpacity onPress={increment}>
-                <Text>{like} likes</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  const increment = () => {
+    setLike(like + 1);
+  };
+  return (
+    <View style={styles.container}>
+      <Image style={styles.img} source={file} />
+      <TouchableOpacity onPress={increment}>
+        <Text>{like} likes</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    text: {
-        fontSize: 22
-    },
-    img: {
-        height: 250,
-        width: 250
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#CDDDDD",
+    borderWidth: 2,
+    marginVertical: 10,
+    paddingVertical: 10,
+    shadowColor: "#000000",
+    shadowOffset: { width: 5, height: 5 },
+    shadowOpacity: 0.3,
+  },
+  text: {
+    fontSize: 22,
+    color: "#000000",
+    marginVertical: 10,
+  },
+  img: {
+    height: 180,
+    width: 180,
+  },
+});
 
-export default Photo
+export default Photo;
--- mobile/instaCat/src/index.js
+++ mobile/instaCat/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import Logo from './components/Logo';
 import Photo from './components/Photo';
 
 const Main = () => {
     return (
-        <>
+        <ScrollView>
             <Logo />
-            <Photo />
-        </>
+            <Photo file={require('./assets/img/cat.png')} count={10} />
+            <Photo file={require('./assets/img/cat2.png')} count={13} />
+        </ScrollView>
     );
 }
 
