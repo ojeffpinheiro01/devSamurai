5418336a34dc85030f299132e35d17b457b59f8c
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-03-09 20:44:28 -0300

    entry list

--- mobile/smartMoney/src/pages/Main/index.js
+++ mobile/smartMoney/src/pages/Main/index.js
@@ -3,12 +3,14 @@ import {View, StyleSheet} from 'react-native';
 
 import BalancePanel from '../../components/BalancePanel'
 import EntrySummary from '../../components/EntrySummary'
+import EntryList from '../../components/EntryList'
 
 const Main = () => {
   return (
     <View style={styles.container}>
       <BalancePanel />
       <EntrySummary />
+      <EntryList />
     </View>
   );
 };
