0dc18dbb7e5828e44296c50a30f2acbfc538beee
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-03-09 22:20:07 -0300

    página de relatórios

--- mobile/smartMoney/index.js
+++ mobile/smartMoney/index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import Main from './src/pages/Main';
-import NewEntry from './src/pages/NewEntry';
+import Report from './src/pages/Report';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => NewEntry);
+AppRegistry.registerComponent(appName, () => Report);
