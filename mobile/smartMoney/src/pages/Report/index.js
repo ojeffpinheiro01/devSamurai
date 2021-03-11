4302e8c79bd09f7e9c25c825c98f9a59aece5d98
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-03-11 20:34:02 -0300

    categorias via props pt2

--- mobile/smartMoney/src/pages/Report/index.js
+++ mobile/smartMoney/src/pages/Report/index.js
@@ -8,6 +8,19 @@ import EntryList from '../../components/EntryList';
 
 const Report = () => {
   const currentBalance = 2064.35
+  const entries = [
+    {key: '1', description: 'Padaria Asa Branca', amount: 10},
+    {key: '2', description: 'Supermercado Isadora', amount: 190},
+    {key: '3', description: 'Posto Ipiranga', amount: 290},
+  ]
+  const entriesGrouped = [
+    {key: '1', description: 'Alimentação', amount: 201},
+    {key: '2', description: 'Combustível', amount: 12},
+    {key: '3', description: 'Aluguel', amount: 120},
+    {key: '4', description: 'Lazer', amount: 250},
+    {key: '5', description: 'Outros', amount: 1200},
+  ]
+
   return (
     <View style={styles.container}>
       <BalanceLabel currentBalance={currentBalance}/>
@@ -19,8 +32,8 @@ const Report = () => {
           <Picker.Item label="Últimos 7 dias" />
         </Picker>
       </View>
-      <EntrySummary />
-      <EntryList />
+      <EntrySummary entriesGrouped={entriesGrouped} />
+      <EntryList entries={entries} />
       <View>
         <Button title="Salvar" />
         <Button title="Fechar" />
