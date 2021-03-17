01c6ac4e8a18e4a98c50ab65f8a25b0c1e86ba0a
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-03-16 22:00:16 -0300

    new entry categorias pt3

--- mobile/smartMoney/src/pages/NewEntry/NewEntryCategoryPicker/index.js
+++ mobile/smartMoney/src/pages/NewEntry/NewEntryCategoryPicker/index.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
-import { View, TouchableOpacity, Text, Modal, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, TouchableOpacity, Text, Modal, StyleSheet, FlatList } from 'react-native'
+
+import { getAllCategories } from '../../../services/Categories'
 
 import Colors from '../../../styles/colors'
 
 const NewEntryCategoryPicker = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const data = await getAllCategories()
+      setCategories(data)
+    }
+    loadCategories()
+  }, [])
 
   return (
     <View>
@@ -14,10 +25,24 @@ const NewEntryCategoryPicker = () => {
         <Text style={styles.pickerButtonText}>Alimentação</Text>
       </TouchableOpacity>
       <Modal animationType="slide" transparent={false} visible={isModalVisible}>
-        <Text>Modal</Text>
-        <TouchableOpacity onPress={() => { setIsModalVisible(false) }}>
-          <Text>x</Text>
-        </TouchableOpacity>
+        <View style={styles.modal}>
+          <FlatList
+            data={categories}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity style={styles.pickerButton}>
+                <Text 
+                  style={[styles.modalItemText, { color: item.color }]}>
+                    {item.name}
+                </Text>
+              </TouchableOpacity>
+            )} />
+          <TouchableOpacity 
+            style={styles.closeButton}
+            onPress={() => { setIsModalVisible(false) }}>
+              <Text style={styles.closeButtonText}>FECHAR</Text>
+          </TouchableOpacity>
+        </View>
       </Modal>
     </View>
   );
@@ -36,6 +61,31 @@ const styles = StyleSheet.create({
     fontSize: 28,
     color: Colors.white,
     textAlign: 'center'
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: Colors.background
+  },
+  modalItemText: {
+    fontSize: 22,
+    color: Colors.white,
+    textAlign: 'center',
+  },
+  closeButton: {
+    alignSelf: 'center',
+    backgroundColor: Colors.background,
+    borderColor: Colors.green,
+    borderWidth: 2,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    paddingVertical: 3,
+    paddingHorizontal: 20,
+  },
+  closeButtonText: {
+    color: Colors.green,
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
 
