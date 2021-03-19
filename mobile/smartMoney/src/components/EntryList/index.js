10b4595ffcc4091d2c2ee55af25da24af8712b3e
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-03-19 18:30:53 -0300

    useEntries

--- mobile/smartMoney/src/components/EntryList/index.js
+++ mobile/smartMoney/src/components/EntryList/index.js
@@ -4,18 +4,12 @@ import { FlatList } from 'react-native'
 import { Container } from '../Core/Container'
 import EntryListItem from './EntryListItem'
 
+import useEntries from '../../hooks/useEntries'
+
 import { getEntries } from '../../services/Entries'
 
 const EntryList = ({days = 7, category, onEntryPress, onPressActionButton}) => {
-  const [entries, setEntries] = useState([])
-
-  useEffect(() => {
-    async function loadEntries() {
-      const data = await getEntries(days, category)
-      setEntries(data)
-    }
-    loadEntries()
-  }, [days, category])
+  const [entries] = useEntries(days, category)
 
   return (
     <Container
