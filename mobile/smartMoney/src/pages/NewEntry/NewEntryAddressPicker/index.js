5b97e97669069a5906dce00513d2ab94024a5b90
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-03-21 18:02:08 -0300

    localização pt2

--- mobile/smartMoney/src/pages/NewEntry/NewEntryAddressPicker/index.js
+++ mobile/smartMoney/src/pages/NewEntry/NewEntryAddressPicker/index.js
@@ -2,26 +2,46 @@ import React from 'react'
 import { Alert, View, TouchableOpacity, StyleSheet } from 'react-native'
 
 import Geolocation from '@react-native-community/geolocation'
+import Geocoder from 'react-native-geocoding'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Colors from '../../../styles/colors'
 
-const NewEntryAddressPicker = () => {
-    const onButtonPress = () => {
-        Geolocation.getCurrentPosition(
-            pos => {
-                const latitude = pos.coords.latitude
-                const longitude = pos.coords.longitude
-                const position = `Lat. ${latitude} - Lon. ${longitude}`
+const getLocation = (latitude, longitude) => {
+    Geocoder.init('AIzaSyBGigREJ4fqi8n4x7-kovxPgh2Xoe3xSj8')
 
-                Alert.alert('Posição', position)
-            },
-            error => { 
-                console.error('NewEntryAddressPicker :: erro:', error)
-            },
-            // { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
-        )
-    }
+    Geocoder.from({ latitude, longitude })
+        .then((json) => {
+            const formattedAddress = json.results[0].formatted_Address
+            Alert.alert('Endereço Formatado', formattedAddress)
+        })
+        .catch((error) => {
+            console.log(
+                'NewEntryAddressPicker :: getLocation :: erro ao recuperar a Localização',
+                error
+            )
+            Alert.alert(
+                'Houve um Erro ao recuperar sua posição, por favor, tenha certeza que autorizou o aplicativo',
+            )
+        })
+}
+
+const getPosition = () => {
+    Geolocation.getCurrentPosition(
+        pos => {
+            const latitude = pos.coords.latitude
+            const longitude = pos.coords.longitude
+            
+            getLocation(latitude, longitude)
+        },
+        error => {
+            console.error('NewEntryAddressPicker :: erro:', error)
+        }
+    )
+}
+
+const NewEntryAddressPicker = () => {
+    const onButtonPress = () => { getPosition() }
     return (
         <View>
             <TouchableOpacity style={styles.button} onPress={onButtonPress}>
