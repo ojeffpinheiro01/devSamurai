bed7fb9877e587c868a8fc460923fee879c58122
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-04-06 00:33:53 -0300

    ajuste

---
+++ frontend/react/todo/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import NewTodo from './components/NewTask';
+import TaskList from './components/TaskList';
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const onNewTask = (value) => {
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: value,
+        checked: false,
+      },
+    ]);
+  };
+
+  const onToggle = (task) => {
+    setTasks(
+      tasks.map((obj) =>
+        obj.id === task.id ? { ...obj, checked: !task.checked } : obj
+      )
+    );
+  };
+
+  const onDelete = (task) => {
+    setTasks(tasks.filter((obj) => obj.id !== task.id));
+  };
+
+  return (
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">todo</h1>
+      </header>
+      <section className="main">
+        <NewTodo onNewTask={onNewTask} />
+        <TaskList tasks={tasks} onToggle={onToggle} onDelete={onDelete} />
+      </section>
+    </section>
+  );
+};
+
+export default App;
